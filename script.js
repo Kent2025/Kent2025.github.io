c23d8a69a30e8e59ce5097fb26c5745283cf7046
Kent2025 <131277946+Kent2025@users.noreply.github.com>
2024-06-11 18:04:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
         "Bin verloren...tief in deinen Augen",
         "Unsere Liebe, ein Feuer...der ewig brennt",
         "Kalbimin tek sahibi sensin",
-        
+        "Deine Stimme so sanft wie die Wellen, so süß wie Zucker",
+        "Meine Liebe zu dir unbeschreiblich",
+        "Du machst mich verrückt",
+        "ich brauche keine Sonne, wenn du bei mir bist",
+        "Wer sucht, findet nicht, aber wer nicht sucht, wird gefunden.",
+        "Ich vermisse dich so sehr",
+        "Ich habe die Liebe erfahren und verstehe jetzt den Sinn des Lebens",
+        "Für die Welt bist du irgendjemand, aber für mich bist du die Welt.",
+        "Mit dir gehe ich überall hin, lass mich nur nie wieder los",
+        "Habe dich in der Mitte meines Herzens graviert",
+        "Du + Ich = Wir♥️",
+        "Entfernung kann nur Körper trennen, aber niemals unsere Herzen",
+        "An dich denken ist schön, doch noch schöner ist dich zu hören. "
     ];
 
     const quoteBox = document.getElementById("quoteBox");
